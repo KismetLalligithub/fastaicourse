7913479c950882c363876ef3b17233dc1fd70437
Kismet Lalli <lalli.kismet@gmail.com>
2025-07-24 16:50:27 -0700

    Fixed practice1.py code

--- practice1.py
+++ practice1.py
@@ -1,4 +1,6 @@
+from fastai.vision.all import * 
 from fastai.collab import * 
-path = untar_data(URLS.ML_SAMPLE)
+path = untar_data(URLs.ML_SAMPLE)
 dls = CollabDataLoaders.from_csv(path/'ratings.csv')
-
+learn = collab_learner(dls, y_range=(0.5, 5.5))
+learn.fine_tune(10)
